7fd0faf17c2e4cc93220fbe90331248002b66135
kaipfitzmann <kai.pfitzmann@orianda.com>
2023-03-06 13:49:29 +0000

    Step 34: Custom Control

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -3,8 +3,9 @@
 sap.ui.define([
   'sap/ui/core/mvc/Controller',
   'sap/ui/core/UIComponent',
-  'sap/ui/core/routing/History'
-], function (Controller, UIComponent, History) {
+  'sap/ui/core/routing/History',
+  'sap/m/MessageToast'
+], function (Controller, UIComponent, History, MessageToast) {
   'use strict'
 
   return Controller.extend('sap.ui.demo.walkthrough.controller.Detail', {
@@ -13,6 +14,7 @@ sap.ui.define([
       oRouter.getRoute('detail').attachPatternMatched(this._onObjectMatched, this)
     },
     _onObjectMatched: function (oEvent) {
+      this.byId('rating').reset()
       this.getView().bindElement({
         path: '/' + window.decodeURIComponent(oEvent.getParameter('arguments').invoicePath),
         model: 'invoice'
@@ -28,6 +30,12 @@ sap.ui.define([
         const oRouter = UIComponent.getRouterFor(this)
         oRouter.navTo('overview', {}, true)
       }
+    },
+    onRatingChange: function (oEvent) {
+      const fValue = oEvent.getParameter('value')
+      const oResourceBundle = this.getView().getModel('i18n').getResourceBundle()
+
+      MessageToast.show(oResourceBundle.getText('ratingConfirmation', [fValue]))
     }
   })
 })
